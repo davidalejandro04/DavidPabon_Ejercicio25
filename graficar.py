89a6405bf7c7c92bc2172674bd2d1b021648a87f
davidalejandro04 <da.pabon10@uniandes.edu.co>
2018-11-06 22:50:56 -0500

    modifico el archivo de graficar para que pueda recibir como parámetro mu y sigma

--- graficar.py
+++ graficar.py
@@ -1,10 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
+mu=float(sys.argv[2])
+sigma=float(sys.argv[3])
+
 from scipy.stats import norm
+
 
 data=np.genfromtxt('sample.dat')
 x_axis = np.arange(-10, 10, 0.001)
-plt.plot(x_axis, norm.pdf(x_axis,0,2))
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
 plt.hist(data,density=True)
 plt.title('PDF real vs datos de sample.dat')
 plt.show()
